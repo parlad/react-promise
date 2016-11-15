b2c1a964a92abe852eecfca3ef774206f9ac14e6
pralad <parlad@outlook.com>
2016-11-16 00:12:31 +0545

    new

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 
-// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
 
 app.use(function (req , res , next) {
 
-  if (req.header['x-forwarded-prot'] === 'http') {
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://'+ req.hostname+req.url);
