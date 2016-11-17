50c66c9c8f97fded6f39a3d4fef8f6dea9fe5ed0
pralad <parlad@outlook.com>
2016-11-17 22:44:24 +0545

    add foundation

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,11 @@ var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
 var Example = require('Example');
+//load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+
+$(document).foundation();
+
 
 ReactDOM.render(
   <Router history = {hashHistory}>
